510dbe4f91ddacb68bda413b9671b4ed8a0370fd
Asanka Nissanka <asanka@getshoutout.com>
2019-02-09 08:43:44 +0530

    udated constants

--- src/Constants.js
+++ src/Constants.js
@@ -4,5 +4,9 @@ module.exports = {
     FULFILMENT_STATES: {
         FULFILLED: 'filfilled',
         FAILED: 'failed'
+    },
+    INVOCATION_SOURCE:{
+        DIALOG_CODE_HOOK:'DialogCodeHook',
+        FULFILMENT_CODE_HOOK:'FulfillmentCodeHook'
     }
 }
