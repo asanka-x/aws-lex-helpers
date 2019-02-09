31a557224fc9e91b2f0486d2d8a3faa1b78e9235
Asanka Nissanka <asanka@getshoutout.com>
2019-02-09 08:32:41 +0530

    added constants

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const Responses = require('./src/Responses');
 const ResponseCard = require('./src/object_types/ResponseCard');
 const ResponseCardAttachment = require('./src/object_types/ResponseCardAttachment');
 const AttachmentButton = require('./src/object_types/AttachmentButton');
+const Constants = require('./src/Constants');
 
 module.exports = {
-    Responses,ResponseCard,ResponseCardAttachment,AttachmentButton
+    Responses,ResponseCard,ResponseCardAttachment,AttachmentButton,Constants
 };
---
+++ src/Constants.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    FULFILMENT_STATES: {
+        FULFILLED: 'filfilled',
+        FAILED: 'failed'
+    }
+}
