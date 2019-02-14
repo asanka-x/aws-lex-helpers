11bc5d007f7c0866d08eaba73f1487ab541dd4b7
Asanka Nissanka <asanka@getshoutout.com>
2019-02-14 14:22:48 +0530

    updated card attachment

--- src/object_types/ResponseCardAttachment.js
+++ src/object_types/ResponseCardAttachment.js
@@ -1,42 +1,45 @@
 'use strict';
 
-class ResponseCardAttachment{
-    constructor(){
+class ResponseCardAttachment {
+    constructor() {
     }
 
-    setTitle(title){
+    setTitle(title) {
         this.title = title;
         return this;
     }
 
-    setSubTitle(subTitle){
+    setSubTitle(subTitle) {
         this.subTitle = subTitle;
         return this;
     }
 
-    setImageUrl(imageUrl){
+    setImageUrl(imageUrl) {
         this.imageUrl = imageUrl;
         return this;
     }
 
-    setAttachmentLinkUrl(attachmentLinkUrl){
+    setAttachmentLinkUrl(attachmentLinkUrl) {
         this.attachmentLinkUrl = attachmentLinkUrl;
         return this;
     }
 
-    setButtons(buttons){
+    setButtons(buttons) {
         this.buttons = buttons;
         return this;
     }
 
-    build(){
-        return {
-            title:this.title,
-            subTitle:this.setSubTitle,
-            imageUrl:this.imageUrl,
-            attachmentLinkUrl:this.attachmentLinkUrl,
-            buttons:this.buttons
+    build() {
+        let cardAttachment = {
+            title: this.title,
+            subTitle: this.setSubTitle,
+            imageUrl: this.imageUrl,
+            attachmentLinkUrl: this.attachmentLinkUrl
         }
+        if (this.buttons) {
+            cardAttachment.buttons = this.buttons;
+        }
+        return cardAttachment;
     }
 
 }
